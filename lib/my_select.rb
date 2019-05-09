bb1031754953f4733c798f0cb6a607cd79c9c72b
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-09 23:25:58 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
 def my_select(collection)
  # your code here!
+if collection.size == 0
+ 	return nil
+ else
+		count = 0
+		new_arr = []
+		while count < collection.count
+			if collection[count] % 2 == 0
+				yield collection[count]
+				new_arr << collection[count]
+				count += 1
+			else
+				puts collection[count]
+				count += 1
+			end
+		end
+		return new_arr
+ 	end
 end
