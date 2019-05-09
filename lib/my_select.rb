61ea605fee502707f352bdf31af52959be99e201
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-09 23:15:02 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,14 +4,9 @@ def my_select(collection)
  count = 0
  while collection.size > count
  
- if collection[counter] % 2 == 0
  yield(collection[count])
  arr << collection[count]
  count += 1
- else
-   puts collection[counter]
-				counter += 1
-				end
  end
  arr
 end
